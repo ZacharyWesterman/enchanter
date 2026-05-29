f39b255bce443f772842e3cf1b4b837c67b311c6
Zachary Westerman <westerman.zachary@gmail.com>
2026-05-28 22:24:37 -0500

    Add documentation

--- src/game.hpp
+++ src/game.hpp
@@ -1,9 +1,21 @@
 namespace enchanter {
 
+/**
+ * @brief The main game loop.
+ */
 class game {
 public:
+	/// @brief Load any files and perform any setup needed before launching.
 	game();
+
+	/**
+	 * @brief Run the game until the user closes it.
+	 */
 	void run();
+
+	/**
+	 * @brief Render a single frame to the window.
+	 */
 	void draw();
 };
 
